a24aea55722fcc296b97554052444a73be6bd666
Predare <predare12000@gmail.com>
2022-01-30 01:21:23 +0300

    unnecessary imports was removed

--- Source/scraper.py
+++ Source/scraper.py
@@ -1,17 +1,11 @@
-import io
-import threading
-
 from requests import get
 from bs4 import BeautifulSoup
 import urllib3
 from threading import Thread
-from requests_html import HTMLSession
 from kivy.clock import Clock
-from functools import partial
 import subprocess
 import sys
 import errno
-import codecs
 
 SEARCH_PARSE = 'String for identification of parsed html page'
 BOOKPAGE_PARSE = 'String for identification of parsed book page'
@@ -38,7 +32,7 @@ def parseBookList(html):
     return dict(zip(bookTitlesParsed,bookLinksParsed))
 
 def downloadBook(link,title):
-    thread = threading.Thread(target=runDownload,args=(link,title))
+    thread = Thread(target=runDownload,args=(link,title))
     thread.start()
     return None
 
@@ -47,12 +41,6 @@ def runDownload(link,title):
         [sys.executable, 'BookDownloader.py'],stdin=subprocess.PIPE, stdout=subprocess.PIPE,encoding='utf-8')
     try:
         linkTitle = '-d {link} ||| {title}'.format(link = link,title = title)
-        #print(linkTitle)
-        #downloadUtil.stdin.write("f")
-        #print(linkTitle)
-        #expLink = 'ААААFФФ'
-        #print('before send: ' + str(expLink)) codecs.encode()
-        print(linkTitle)
         answer = downloadUtil.communicate(linkTitle)
         print(answer[0])
 
@@ -66,8 +54,3 @@ def runDownload(link,title):
             print(e)
             # Raise any other error.
             raise
-
-    #stdout_data = downloadUtil.communicate(input='data_to_write')[0]
-
-    #downloadUtil.stdin.write('exit')
-    #downloadUtil.stdin.flush()
