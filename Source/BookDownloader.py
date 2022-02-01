9780f75a12002e3729d0e83d660893a7d2ced6cf
Predare <predare12000@gmail.com>
2022-02-01 03:19:58 +0300

    Unstable version, try to put all loadings in one process

--- Source/BookDownloader.py
+++ Source/BookDownloader.py
@@ -9,16 +9,19 @@ from requests_html import HTMLSession
 import sys
 import codecs
 from pathlib import Path
+import asyncio
 
-poolM = urllib3.PoolManager()
+def runAsyncio():
+    pass
+    #loop.run_forever()
 
 def downloadBook(link, title, *largs):
-    with HTMLSession() as session:
-        response = session.get(link)
-        response.html.render()
-        response.close()
+    session = HTMLSession()
+    response = session.get(link)
+    response.html.render()
+    response.close()
     downloadLink = findDownloadLink(response)
-    loading(downloadLink, title)
+    task = asyncio.ensure_future(loading(downloadLink, title))
 
 def findDownloadLink(html):
     data_bid = html.html.find('div.bookpage--chapters.player--chapters', first = True).attrs['data-bid']
@@ -30,12 +33,9 @@ def findDownloadLink(html):
 
     return downloadLink
 
-def newDownloadThread(link, filename, path = ''):
-    download_thread = threading.Thread(target=loading, args=(link,filename,path))
-    download_thread.start()
+async def loading(link,filename, path = os.path.expanduser('~/Baudis/SavedBooks')):
 
-def loading(link,filename, path = os.path.expanduser('~/Baudis/SavedBooks')):
-    response = poolM.request('GET',link,preload_content = False)
+    response = await poolM.request('GET',link,preload_content = False)#Get file link
     Path(path ).mkdir(parents=True, exist_ok=True)#Create directory for books if not exist
     contentLength = response.getheader('Content-Length')
     chunkSize = 1024
@@ -44,30 +44,35 @@ def loading(link,filename, path = os.path.expanduser('~/Baudis/SavedBooks')):
     inCompletePercent = 0;
     filename = filename.replace(' ','_')
 
+    #Create file and write in
     with open(R'{path}/{filename}.mp3'.format(path=path, filename=filename),'wb') as out_file:
-        for chunk in response.stream(chunkSize):
+        for chunk in await response.stream(chunkSize):
             if chunk:
                 inCompletePercent += chunkSize
                 if inCompletePercent >= filePercent:
                     loadPercent += 1
                     inCompletePercent -= filePercent
                     print('Load: {percent}%'.format(percent = loadPercent))
-                out_file.write(chunk)
+                await out_file.write(chunk)
     print(f' Download end: \n Link: {link} \n Filename: {filename} \n Filepath: {path}/{filename}.mp3')
     print('Close downloader')
 
+poolM = urllib3.PoolManager()
+loop = asyncio.get_event_loop()
+thread = Thread(target=runAsyncio, args=(), daemon=True)
+thread.start()
 
 while True:
-    prefix = '-d'
+    # Listening and executing commands from main process
     LinkTitle = sys.stdin.readline()
-    if(LinkTitle.strip() == 'cancel'):
-        print('Canceled')
-    elif prefix in LinkTitle:
-        LinkTitle = LinkTitle.removeprefix(prefix)
+    if(LinkTitle.strip() == 'close'):
+        session.close()
+        thread.close()
+        loop.stop()
+        print('Closed')
+    elif '-d' in LinkTitle:
+        LinkTitle = LinkTitle.removeprefix('-d')
         LinkTitle = LinkTitle.split(' ||| ')
         downloadBook(LinkTitle[0],LinkTitle[1])
     else: print('Unknown command {LinkTitle}'.format(LinkTitle = LinkTitle))
-    sys.stdin.flush()
-    sys.stdin.close()
-    break
 
